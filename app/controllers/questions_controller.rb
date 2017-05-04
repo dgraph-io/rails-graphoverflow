46cf5fba7ec1b3d2a64a313921ca6d38ae8247e1
Sung Won Cho <mikeswcho@gmail.com>
2017-05-04 18:11:25 +1000

    Implement CRUD

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,38 +1,31 @@
 require 'dgraph_client'
 
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :edit]
+
   def index
     query = %q(
 {
-  questions(func: gt(count(question_body), 0)) {
+  questions(func: gt(count(question_body), 0), first: 20) {
+    _uid_
     question_body
+    question_title
   }
 }
 )
-    client = ::DgraphClient.new()
-    json = client.do(query)
-
-    @questions = json.fetch(:questions, [])
-  end
-
-  def show
-    query = %Q(
-{
-  question(id:#{params[:id]}) {
-    question_body
-  }
-}
-)
-
     client = ::DgraphClient.new()
     json = client.do(query)
 
     puts json
 
-    @question = json[:question][0]
+    @questions = json.fetch(:questions, [])
+  end
+
+  def show
   end
 
   def new
+    @question = {}
   end
 
   def edit
@@ -57,28 +50,56 @@ mutation {
   end
 
   def update
+    question_id = params[:id]
+    query = %Q(
+mutation {
+  set {
+    <#{question_id}> <question_body> "#{params[:question][:question_body]}" .
+    <#{question_id}> <question_title> "#{params[:question][:question_title]}" .
+  }
+}
+)
+
+    client = ::DgraphClient.new()
+    client.do(query)
+
     respond_to do |format|
-      if @question.update(question_params)
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
-        format.json { render :show, status: :ok, location: @question }
-      else
-        format.html { render :edit }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
+      format.html { redirect_to action: 'show', id: question_id }
     end
   end
 
   def destroy
-    @question.destroy
+    query = %Q(
+mutation {
+  delete {
+    <#{params[:id]}> * * .
+  }
+}
+)
+
+    client = ::DgraphClient.new()
+    json = client.do(query)
+
     respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to action: 'index' }
     end
   end
 
   private
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def question_params
-      params.fetch(:question, {})
+    def set_question
+      query = %Q(
+{
+  question(id:#{params[:id]}) {
+    _uid_
+    question_body
+    question_title
+  }
+}
+)
+
+      client = ::DgraphClient.new()
+      json = client.do(query)
+
+      @question = json[:question][0]
     end
 end
