60d8c2e66b4e1a65d168fbc32c8e512f8220c12a
Sung Won Cho <mikeswcho@gmail.com>
2017-05-30 15:30:36 +0800

    Fix for 0.7.7

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -43,7 +43,7 @@ class QuestionsController < ApplicationController
       answer.upvoted_by @filter(var(user)) {
         user_name
       }
-      count(answer.upvoted_by)
+      upvote_count: count(answer.upvoted_by)
     }
   }
 }
